876a13354d3280c5a8cbb3051e9a05495d3a4251
annasaban <118971115+annasaban@users.noreply.github.com>
2023-02-18 20:08:57 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+let box = 'Hej!';
+let user = 'Anna';
+let id = 0;
+
+let message;
+
+message = 'Hello!';
+
+message = 'Innopolis';
+
+let hej = 'Hej varlden!';
+
+let newMessage1 = hej;
+
+const myBirthday = '07/20';
+
+console.log(myBirthday);
+
+let newMessage2 = 'hello';
+
+newMessage2 = 7654321;
+
+const bigInt = 88888888888888888888888888888n;
+
+let nameChecked = true;
+let ageChecked = false;
+
+let age = null;
+
+let newAge;
+
+const a = {
+
+}
+
+
+
+
